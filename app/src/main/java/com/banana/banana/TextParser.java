b42298691ceea5b514564f8526bfe148e7f9f31a
abbyvansoest <asoest@princeton.edu>
2017-05-09 13:13:02 -0400

    leif's parsing corrections

--- app/src/main/java/com/banana/banana/TextParser.java
+++ app/src/main/java/com/banana/banana/TextParser.java
@@ -4,13 +4,10 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.lang.*;
-
-
 import static android.content.ContentValues.TAG;
 
 
@@ -22,9 +19,6 @@ public class TextParser {
         Log.v(TAG, "Here comes the input: ");
         Log.v(TAG, input);
         List<String> lines = new ArrayList<>(Arrays.asList(input.split("\n")));
-        //return realParse(lines);
-        Log.v(TAG, "LINES INCOMING");
-        Log.v(TAG, lines.toString());
         return realParse(lines);
     }
 
@@ -32,9 +26,9 @@ public class TextParser {
     // This is used to create the checklist while the OCR output is unusable
     private static List<List<String>> fakeParse(List<String> lines) {
         List<List<String>> checklistOutput = new ArrayList<>();
-        for(String line: lines) {
-            List<String> words = new ArrayList<>(Arrays.asList(line.split(" "))); // ASSUMPTION: ITEMS SEPARATED BY 1 SPACE
-            if(words.size() >= 3) {
+        for (String line : lines) {
+            List<String> words = new ArrayList<>(Arrays.asList(line.split(" ")));
+            if (words.size() >= 3) {
                 List<String> outputLine = new ArrayList<>();
                 outputLine.add(0, words.get(0));
                 outputLine.add(1, words.get(1));
@@ -51,18 +45,21 @@ public class TextParser {
         List<Map<String, String>> wordTypes = new ArrayList<>();
 
         // Get the set of words in each line of the receipt
-        for(String line: lines) {
-            List<String> words = new ArrayList<>(Arrays.asList(line.split(" "))); // ASSUMPTION: ITEMS SEPARATED BY 1 SPACE
+        for (String line : lines) {
+            List<String> words = new ArrayList<>(Arrays.asList(line.split(" ")));
 
             // Determine and store the type of each of those words (price, title, or amount)
             Map<String, String> curTypes = new LinkedHashMap<>();
-            for(String word : words) {
+            for (String word : words) {
                 String category = categorizeWord(word);
 
                 // Replace all incorrect, guessable characters for prices
-                if(category.equals("Price")) {
+                if (category.equals("Price")) {
                     String newWord = correctErrors(word);
-                    curTypes.put(newWord, category);
+                    if (newWord.equals("."))
+                        curTypes.put("0.00", category);
+                    else
+                        curTypes.put(newWord, category);
                 } else {
                     curTypes.put(word, category);
                 }
@@ -74,11 +71,7 @@ public class TextParser {
         }
 
         // Parse each line-worth of word types to find relevant lines
-        for(Map<String, String> typeSet : wordTypes){
-
-            // Only use lines that have a price in them
-            //if (typeSet.values().contains("Price")) {
-
+        for (Map<String, String> typeSet : wordTypes) {
             // Take the elements of a valid line and store them in the following order:
             // 1: Title  2: Quantity  3: Price
             List<String> outputLine = new ArrayList<>();
@@ -86,34 +79,27 @@ public class TextParser {
             outputLine.add(1, "1");
             outputLine.add(2, "0.00");
             for (String word : typeSet.keySet()) {
-                if(typeSet.get(word).equals("Title")) {
+                if (typeSet.get(word).equals("Title")) {
                     outputLine.set(0, outputLine.get(0) + " " + word);
-                }
-                else if (typeSet.get(word).equals("Quantity")) {
-                    //outputLine.set(1, word);
+                } else if (typeSet.get(word).equals("Quantity")) {
                     outputLine.set(0, outputLine.get(0) + " " + word);
-                }
-                else if (typeSet.get(word).equals("Price")) {
-                    // o/O -> 0, i/I -> 1, s/S-> 5
+                } else if (typeSet.get(word).equals("Price")) {
                     outputLine.set(2, word);
                 }
             }
             checklistOutput.add(outputLine);
-            //}
         }
 
         return checklistOutput;
     }
 
     private static String categorizeWord(String word) {
-        if(word.contains(String.valueOf('$')) || word.contains(String.valueOf('.')) || word.contains(String.valueOf(','))
-                || word.contains(String.valueOf("'")) || word.contains(String.valueOf("-"))) {
+        if (word.contains(String.valueOf('$')) || word.contains(String.valueOf('.')) || word.contains(String.valueOf(','))
+                || word.contains(String.valueOf("'")) || word.contains(String.valueOf("-")) || word.contains(String.valueOf("~"))) {
             return "Price";
-        }
-        else if (isNumeric(word)) {
+        } else if (isNumeric(word)) {
             return "Quantity";
-        }
-        else {
+        } else {
             return "Title";
         }
     }
@@ -126,7 +112,9 @@ public class TextParser {
         String newWord = word;
         newWord = newWord.replaceAll(",", ".");
         newWord = newWord.replaceAll("'", ".");
+        newWord = newWord.replaceAll("..", ".");
         newWord = newWord.replaceAll("-", ".");
+        newWord = newWord.replaceAll("~", ".");
         newWord = newWord.replaceAll("$", "");
         newWord = newWord.replaceAll("o", "0");
         newWord = newWord.replaceAll("O", "0");
@@ -135,15 +123,9 @@ public class TextParser {
         newWord = newWord.replaceAll("i", "1");
         newWord = newWord.replaceAll("I", "1");
         newWord = newWord.replaceAll("l", "1");
-
-//        for (int i = 0; i < newWord.length(); i++){
-//            char c = newWord.charAt(i);
-//            if(!Character.isDigit(c) && c != '.') {
-//                newWord = newWord.replaceAll(String.valueOf(c), "");
-//            }
-//        }
+        newWord = newWord.replaceAll("H", "A");
+        newWord = newWord.replace("\\", "");
+        newWord = newWord.replaceAll("[^\\d.]", "");
         return newWord;
     }
 }
-
-
