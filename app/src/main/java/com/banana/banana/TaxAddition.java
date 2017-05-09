b79bc993e51fd67b746c5a8e44a84eb62780976c
abbyvansoest <asoest@princeton.edu>
2017-05-09 08:31:59 -0400

    adding more tip/tax stuff

--- app/src/main/java/com/banana/banana/TaxAddition.java
+++ app/src/main/java/com/banana/banana/TaxAddition.java
@@ -9,10 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 import static com.banana.banana.EditReceipt.ITEM_ID;
 import static com.banana.banana.EditReceipt.PRICE_ID;
 
-public class TaxAddition extends AppCompatActivity {
+public class TaxAddition extends AppCompatActivity  {
 
     private EditText editTax;
     private EditText editTip;
@@ -29,19 +35,37 @@ public class TaxAddition extends AppCompatActivity {
         editTax.setText("15", TextView.BufferType.EDITABLE);
         editTip.setText("20", TextView.BufferType.EDITABLE);
 
+
         // button
         final Button done =(Button) findViewById(R.id.editDone);
+        Set<String> names = ((MyList) getApplication()).getUsers();
+        final ExpandableListDataPump pump = new ExpandableListDataPump(this, names);
         done.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-               Intent finalIntent = new Intent(TaxAddition.this, MainActivity.class);
+                // get list of final prices and total price
+
+                pump.getData();
+                HashMap<String, Double> finalPrices = pump.getTotalPrices();
+                double total = pump.getTotal();
+
+                // get list of perc
+                // add tip tax proportionately to each
+                HashMap<String, Double> perc = new HashMap<String, Double>();
+                for (String name : finalPrices.keySet()) {
+                    perc.put(name, finalPrices.get(name)/total);
+                }
+
+                pump.addTipTax(editTip.getText().toString(), editTax.getText().toString(), perc);
+
+                Intent finalIntent = new Intent(TaxAddition.this, MainActivity.class);
                 startActivity(finalIntent);
+
             }
         });
 
     }
-
 
 }
 
