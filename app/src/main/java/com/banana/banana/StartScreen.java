d3c73e14c1e7404b310df496ed3b393aa5f09a5e
Jacob Whitlow <whitlow@princeton.edu>
2017-05-09 17:18:57 -0400

    Starting screen with banana, hopefully checks for permissions.  Also fixes a couple bugs in previous commits.

--- app/src/main/java/com/banana/banana/StartScreen.java
+++ app/src/main/java/com/banana/banana/StartScreen.java
@@ -1,10 +1,16 @@
 package com.banana.banana;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jacob on 5/9/17.
@@ -12,57 +18,79 @@ import android.support.v7.app.AppCompatActivity;
 
 public class StartScreen extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_CONTACTS = 0;
-    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
-    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 2;
-    private static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE = 3;
-    private static final int MY_PERMISSIONS_REQUEST_READ_PHONE = 4;
+    private static final int MY_PERMISSIONS_REQUEST = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_start);
+        boolean shouldStay = false;
+        List<String> permissionRequests = new ArrayList<>();
+        String [] array = new String[5];
 
+        // Figure out all of the current permssions granted
         PackageManager pm = this.getPackageManager();
         int hasPermCont = pm.checkPermission(
                 Manifest.permission.READ_CONTACTS,
                 this.getPackageName());
         if (hasPermCont != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CONTACTS},
-                    MY_PERMISSIONS_REQUEST_CONTACTS);
+            permissionRequests.add(Manifest.permission.READ_CONTACTS);
+            shouldStay = true;
         }
         int hasPermCam = pm.checkPermission(
                 Manifest.permission.CAMERA,
                 this.getPackageName());
         if (hasPermCam != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
+            permissionRequests.add(Manifest.permission.CAMERA);
+            shouldStay = true;
         }
         int hasPermSMS = pm.checkPermission(
                 Manifest.permission.SEND_SMS,
                 this.getPackageName());
         if (hasPermSMS != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+            permissionRequests.add(Manifest.permission.SEND_SMS);
+            shouldStay = true;
         }
         int hasPermStore = pm.checkPermission(
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 this.getPackageName());
         if (hasPermStore != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+            permissionRequests.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            shouldStay = true;
         }
         int hasPermNumb = pm.checkPermission(
                 Manifest.permission.READ_PHONE_STATE,
                 this.getPackageName());
-        if (hasPermCam != PackageManager.PERMISSION_GRANTED) {
+        if (hasPermNumb != PackageManager.PERMISSION_GRANTED) {
+            permissionRequests.add(Manifest.permission.READ_PHONE_STATE);
+            shouldStay = true;
+        }
+
+        if (shouldStay) {
+            // If not all permission granted, request them
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    MY_PERMISSIONS_REQUEST_READ_PHONE);
+                    permissionRequests.toArray(array),
+                    MY_PERMISSIONS_REQUEST);
+        } else {
+            // If all necessary permissions granted, move to main app
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                    Intent mainIntent = new Intent(StartScreen.this, OpenCamera.class);
+                    StartScreen.this.startActivity(mainIntent);
+                    StartScreen.this.finish();
+                }
+            }, 2000);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST) {
+            // Restart this activity after requesting permissions to see if they were granted
+            Intent restart = new Intent(StartScreen.this, StartScreen.class);
+            startActivity(restart);
         }
     }
 
