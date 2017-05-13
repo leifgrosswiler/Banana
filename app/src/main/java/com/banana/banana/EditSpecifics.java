fa5df709428a203e85cf67d77fceaa6330530eef
David Prilutsky <dyp@princeton.edu>
2017-05-13 13:52:40 -0400

    hmmm

--- app/src/main/java/com/banana/banana/EditSpecifics.java
+++ app/src/main/java/com/banana/banana/EditSpecifics.java
@@ -68,16 +68,12 @@ public class EditSpecifics extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Not a price...", Toast.LENGTH_SHORT).show();
             return;
         }
-        if (newItem.isEmpty() || newItem.trim().isEmpty()) {
-            Toast.makeText(getApplicationContext(),"Order needs a name...", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
         Order updated = new Order(newItem, newPrice, position,OrderData.getAt(position).getNumPpl());
         try {
             OrderData.set(position, updated);
         } catch(Exception e) {
-            System.out.println("position is incorrect");
+            System.out.println("position is fucked up");
         }
 
         Intent intentUpdate = new Intent(EditSpecifics.this, MainReceipt.class);
