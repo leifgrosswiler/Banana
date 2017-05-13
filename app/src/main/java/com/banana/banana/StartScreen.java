cd83333d00ac65589aa6df1c838617fd804fe005
Jacob Whitlow <whitlow@princeton.edu>
2017-05-13 19:14:12 -0400

    moves gathering of contacts to start screen

--- app/src/main/java/com/banana/banana/StartScreen.java
+++ app/src/main/java/com/banana/banana/StartScreen.java
@@ -1,11 +1,14 @@
 package com.banana.banana;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
@@ -13,7 +16,11 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.banana.banana.SelectItems.everything;
 
 /**
  * Created by Jacob on 5/9/17.
@@ -51,6 +58,8 @@ public class StartScreen extends AppCompatActivity {
             permissionRequests.add(Manifest.permission.READ_CONTACTS);
             shouldStay = true;
             pCount++;
+        } else if (everything == null) {
+            getContactNames();
         }
         int hasPermCam = pm.checkPermission(
                 Manifest.permission.CAMERA,
@@ -103,6 +112,81 @@ public class StartScreen extends AppCompatActivity {
                 }
             }, 2000);
         }
+    }
+
+    private void getContactNames() {
+
+        List<CoolList> all = new ArrayList<>();
+        Map<String, ArrayList<String>> tempAll = new HashMap<>();
+
+        String[] EMAIL_PROJECTION = new String[] {
+                ContactsContract.CommonDataKinds.Email.CONTACT_ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Email.DATA
+        };
+
+        String[] PHONE_PROJECTION = new String[] {
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.DATA
+        };
+
+        ContentResolver cr = getContentResolver();
+        Cursor cursor = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, EMAIL_PROJECTION, null, null, null);
+        Cursor phoneCursor = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PHONE_PROJECTION, null, null, null);
+
+        if (cursor != null && phoneCursor != null) {
+            try {
+                final int displayNameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                final int emailIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
+                final int displayNameIndexNum = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                final int phoneIndex = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA);
+
+                String displayName, address, displayNameNum, number;
+
+                while (cursor.moveToNext()) {
+                    ArrayList<String> curContact = new ArrayList<>();
+                    displayName = cursor.getString(displayNameIndex);
+                    address = cursor.getString(emailIndex);
+                    curContact.add(0, address);
+                    curContact.add(1, null);
+                    tempAll.put(displayName, curContact);
+                }
+
+                while (phoneCursor.moveToNext()) {
+                    displayNameNum = phoneCursor.getString(displayNameIndexNum);
+                    number = phoneCursor.getString(phoneIndex);
+                    ArrayList<String> curContact = tempAll.get(displayNameNum);
+
+                    if (curContact != null) {
+                        curContact.add(1, number);
+                        tempAll.put(displayNameNum, curContact);
+                    }
+                    else {
+                        curContact = new ArrayList<>();
+                        curContact.add(0, null);
+                        curContact.add(1, number);
+                        tempAll.put(displayNameNum, curContact);
+                    }
+                }
+
+            } finally {
+                cursor.close();
+                phoneCursor.close();
+            }
+        }
+
+        for(String key : tempAll.keySet()) {
+            CoolList curContact = new CoolList();
+            String curEmail = tempAll.get(key).get(0);
+            String curPhone = tempAll.get(key).get(1);
+            curContact.add(key);
+            curContact.add(curEmail); // email
+            curContact.add(curPhone); // number
+            all.add(curContact);
+        }
+
+        everything = all;
     }
 
     @Override
